6b42a1fc96f398b1c46868a90ae1fe8ea9c3aef0
ThomasBoxall <38958967+ThomasBoxall@users.noreply.github.com>
2023-03-12 16:44:41 +0000

    start home page

---
+++ global/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="header-name">
+        <a class="mono" href="#"><span class="theme-colour">&lt;</span>Thomas Boxall<span class="theme-colour">/&gt;</span></a>
+    </div>
+    <div class="header-nav-elements">
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Contact</a>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thomas Boxall | Home</title>
+    <link rel="stylesheet" href="/global/global.css">
+    <link rel="stylesheet" href="home-styles.css">
+</head>
+<body>
+    <!-- <?php include($_SERVER['DOCUMENT_ROOT'] . "/global/header.php");?> -->
+    <main>
+        <h1 class="mono home-hero"><span class="theme-colour">&lt;</span>Thomas Boxall<span class="theme-colour">/&gt;</span></h1>
+        <p class="home-subtitle">Can sometimes make computers do things</p>
+        <div class="home-buttons">
+            <a class="home-button-single" href="#">
+                <p>More about me</p>
+            </a>
+            <a class="home-button-single" href="#">
+                <p>My projects</p>
+            </a>
+        </div>
+    </main>
+</body>
+</html>
